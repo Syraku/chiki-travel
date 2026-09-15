a9e03000690e820fd683221354725abe18f65b09
Syraku <sirakkun@gmail.com>
2026-09-15 21:50:19 +0700

    chore(sanity): connect studio to CHIKI TRAVEL project

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -5,7 +5,7 @@ import { schemaTypes } from "./schemaTypes";
 export default defineConfig({
   name: "chiki-travel",
   title: "CHIKI TRAVEL CMS",
-  projectId: process.env.SANITY_STUDIO_PROJECT_ID || "",
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID || "y90hp7sc",
   dataset: process.env.SANITY_STUDIO_DATASET || "production",
   plugins: [structureTool()],
   schema: {
