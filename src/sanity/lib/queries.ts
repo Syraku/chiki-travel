491bb1f023ff4b56297235b330d6d823eba96e1a
Syraku <sirakkun@gmail.com>
2026-09-15 21:54:53 +0700

    fix: align Sanity queries with CMS schemas

--- src/sanity/lib/queries.ts
+++ src/sanity/lib/queries.ts
@@ -1,8 +1,9 @@
 export const fleetQuery = `*[_type == "fleet"] | order(_createdAt asc) {
   _id,
-  vehicleName,
+  name,
   image,
-  description
+  description,
+  specifications
 }`;
 
 export const siteSettingsQuery = `*[_type == "siteSettings"][0] {
@@ -17,5 +18,6 @@ export const routesQuery = `*[_type == "route"] | order(_createdAt asc) {
   _id,
   origin,
   destination,
-  description
+  description,
+  ctaMessage
 }`;
