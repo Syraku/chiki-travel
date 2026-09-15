0ad612bc52d5a215e999393e1dea3c90ea26ae24
Syraku <sirakkun@gmail.com>
2026-09-15 22:04:13 +0700

    fix: use Sanity image URL package

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,7 +1,7 @@
-import { createImageUrlBuilder } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url";
 import { sanityClient } from "./client";
 
-const builder = createImageUrlBuilder(sanityClient);
+const builder = imageUrlBuilder(sanityClient);
 
 export function urlForImage(source: unknown) {
   return builder.image(source as Parameters<typeof builder.image>[0]);
